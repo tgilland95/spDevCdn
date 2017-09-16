e4cf995207ad5d74dc086527f4c95fe7957ae59b
Tyler <tylergilland@gmail.com>
2017-09-15 21:10:28 -0600

    auto-pushFri Sep 15 2017 21:10:28 GMT-0600 (Mountain Daylight Time)

--- main.bundle.js
+++ main.bundle.js
@@ -8838,17 +8838,16 @@
 	var appWebUrl = '';
 	
 	function init() {
-	    // This code runs when the DOM is ready and creates a context object which is
-	    // needed to use the SharePoint object model
-	    $(document).ready(function () {
+	      // This code runs when the DOM is ready and creates a context object which is
+	      // needed to use the SharePoint object model
+	      $(document).ready(function () {
 	
-	        // parses hostweb and appweb URLs from page URL
-	        hostWebUrl = decodeURIComponent((0, _Utils.getQueryStringParameter)("SPHostUrl"));
-	        appWebUrl = decodeURIComponent((0, _Utils.getQueryStringParameter)("SPAppWebUrl"));
-	        // loads index.aspx page
-	        indexPage.run(hostWebUrl, appWebUrl);
-	        alert("hi and bye");
-	    });
+	            // parses hostweb and appweb URLs from page URL
+	            hostWebUrl = decodeURIComponent((0, _Utils.getQueryStringParameter)("SPHostUrl"));
+	            appWebUrl = decodeURIComponent((0, _Utils.getQueryStringParameter)("SPAppWebUrl"));
+	            // loads index.aspx page
+	            indexPage.run(hostWebUrl, appWebUrl);
+	      });
 	}
 
 /***/ },
