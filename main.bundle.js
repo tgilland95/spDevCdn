a2e8d5a6da632a064f58c200ece16f9c54c89586
Tyler <tylergilland@gmail.com>
2017-09-18 14:33:56 -0600

    auto-pushMon Sep 18 2017 14:33:56 GMT-0600 (Mountain Daylight Time)

--- main.bundle.js
+++ main.bundle.js
@@ -9339,7 +9339,7 @@
 	                options += categoryList[i];
 	                options += '</option>';
 	              }
-	              $('#r-cat').val(options);
+	              $('#r-cat').val(tempRowData.recordCategory);
 	              $('#r-dept').val(tempRowData.depNumber);
 	              $('#r-code').val(tempRowData.code);
 	              $('#r-type').val(tempRowData.recordType);
