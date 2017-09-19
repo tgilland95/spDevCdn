b86ea4cb4e7f21b49919f9821ae91b503526317f
Tyler <tylergilland@gmail.com>
2017-09-19 13:47:04 -0600

    auto-push drs Tue Sep 19 2017 13:47:04 GMT-0600 (Mountain Daylight Time)

--- drsmain.bundle.js
+++ drsmain.bundle.js
@@ -9497,7 +9497,7 @@
 	
 	                            // if user added new message to admin, Status will be set to 'Pending'
 	                            var notifyAdmin = 0;
-	                            if (newAdminMsg != (initialUserCmts || '')) {
+	                            if (newAdminMsg !== initialUserCmts || newAdminMsg !== '') {
 	                                notifyAdmin = 1;
 	                            }
 	
