82f84e6502f7b8d09fdac55676858fff898e1189
tgilland95 <tylergilland@gmail.com>
2018-01-15 15:54:11 -0700

    auto

--- drsmain.bundle.js
+++ drsmain.bundle.js
@@ -9826,6 +9826,7 @@
 	              console.log(state);
 	              var canSave = Object.values(state).toString() === Object.values(getCurrentState()).toString();
 	              console.log(canSave);
+	              $('#saveRecord').prop('disabled', canSave);
 	              // if common record, don't allow user to change Record Type, Category, or
 	              // Function
 	              if ($(this).closest('tr').children()[0].innerText.charAt(0) == 'C') {
