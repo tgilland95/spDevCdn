6c52b2418193a725564d2cd59c75eeb0e8bf67a1
tgilland95 <tylergilland@gmail.com>
2018-01-15 17:20:03 -0700

    auto

--- drsmain.bundle.js
+++ drsmain.bundle.js
@@ -9820,7 +9820,7 @@
 	              console.log('current: ----> ' + getCurrentState());
 	              var canSave = Object.values(state).toString() === Object.values(getCurrentState()).toString();
 	              console.log(canSave);
-	              console.log('-----------------------------4--------------------------------');
+	              console.log('revision', '-------------------------', '5');
 	              $('#saveRecord').prop('disabled', canSave);
 	              // if common record, don't allow user to change Record Type, Category, or
 	              // Function
