8b7c6f1f8abbf3f3d357a9510e998e6e626c4e12
tgilland95 <tylergilland@gmail.com>
2018-01-16 11:32:22 -0700

    auto

--- drsmain.bundle.js
+++ drsmain.bundle.js
@@ -9687,7 +9687,7 @@
 	              window.getCurrentState = getCurrentState;
 	              $('#editForm').change(function () {
 	                var latestState = getCurrentState();
-	                if (latestState[1] === "" || latestState[2] === "" || latestState[2] === "Select a category" || latestState[3] === "" || latestState[3] === "Select a category") {
+	                if (latestState[1] === "" || latestState[2] === "" || latestState[2] === "Select a category" || latestState[2] === null || latestState[3] === "" || latestState[3] === "Select a category" || latestState[3] === null) {
 	                  return;
 	                }
 	                $('#saveRecord').prop('disabled', false);
